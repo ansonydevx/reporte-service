29dae6b75b6f52e8ab41d6e5d9e3a141f92eb8d5
Ansony <ansonydevx@gmail.com>
2025-12-28 21:19:03 -0500

    init

---
+++ src/test/java/com/onclass/reporte/ReporteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onclass.reporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
